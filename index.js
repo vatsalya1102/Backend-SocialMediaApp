2c5cae12a26511756f231fa2020c732a20dfa4a1
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-05-15 15:24:06 +0530

    Added authentication, GoogleAuth, and user actions Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 //Now we use express middleware to connect this to our application
 
 const app = express();
@@ -15,7 +16,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 //cors should be used before postRoutes
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 //This means every route in postRoutes will start with /posts
 
 const PORT = process.env.PORT || 5000;
